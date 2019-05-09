ec34e724566e4dc2239502051439ab550e90b751
Chris Bell <chbell@practicevelocity.com>
2019-05-09 10:45:39 -0500

    working vscode launch.json config

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,6 +4,77 @@ module.exports = class extends Generator {
   constructor(args, opts) {
     // Calling the super constructor is important so our generator is correctly set up
     super(args, opts);
+
+    this.setUpVsCodeFiles = function () {
+      this.fs.copy(
+        this.templatePath('./.vscode/'),
+        this.destinationPath('./.vscode/'),
+        { appName: this.answers.name }
+      );
+
+      const launchJson = {
+        "version": "0.2.0",
+        "configurations": [
+          {
+            "name": ".NET Core Launch (silent)",
+            "type": "coreclr",
+            "request": "launch",
+            "preLaunchTask": "build",
+            "program": `\${workspaceFolder}/bin/Debug/netcoreapp2.2/${this.answers.name}.dll`,
+            "args": [],
+            "cwd": "${workspaceFolder}",
+            "stopAtEntry": false,
+            "internalConsoleOptions": "neverOpen",
+            "launchBrowser": {
+              "enabled": false,
+              "args": "${auto-detect-url}",
+              "windows": {
+                "command": "cmd.exe",
+                "args": "/C start ${auto-detect-url}"
+              },
+              "osx": {
+                "command": "open"
+              },
+              "linux": {
+                "command": "xdg-open"
+              }
+            },
+            "env": {
+              "ASPNETCORE_ENVIRONMENT": "Development",
+              "EncryptionPublicKey": "",
+              "KeyFilePath": "",
+              "PathBase": ""
+            },
+            "sourceFileMap": {
+              "/Views": "${workspaceFolder}/Views"
+            }
+          }
+        ]
+      }
+
+      const tasksJson = {
+        "version": "2.0.0",
+        "tasks": [
+          {
+            "label": "build",
+            "command": "dotnet",
+            "type": "process",
+            "args": [
+              "build",
+              `\${workspaceFolder}/${this.answers.name}.csproj`
+            ],
+            "problemMatcher": "$msCompile",
+            "presentation": {
+              "echo": false,
+              "reveal": "silent"
+            }
+          }
+        ]
+      }
+
+      this.fs.extendJSON(this.destinationPath('./.vscode/launch.json'), launchJson);
+      this.fs.extendJSON(this.destinationPath('./.vscode/tasks.json'), tasksJson);
+    }
   }
 
   async prompting() {
@@ -13,7 +84,22 @@ module.exports = class extends Generator {
         name: "name",
         message: "App name (default in parentheses):",
         default: this.appname
-      }
+      },
+      // {
+      //   type: "list",
+      //   name: "ui-framework",
+      //   message: "Select UI framework",
+      //   choices: [
+      //     "Angular Material",
+      //     "DevExtreme"
+      //   ]
+      // },
+      // {
+      //   type: "confirm",
+      //   name: "include-auth",
+      //   message: "Include Authentication?",
+      //   default: true
+      // }
     ]);
   }
 
@@ -31,7 +117,7 @@ module.exports = class extends Generator {
     );
 
     this.fs.copyTpl(
-      this.templatePath('./'),
+      this.sourceRoot(),
       this.destinationRoot(),
       { appName: this.answers.name }
     );
@@ -43,13 +129,12 @@ module.exports = class extends Generator {
       { appName: this.answers.name }
     );
 
-    this.fs.copy(
-      this.templatePath('./.vscode/'),
-      this.destinationPath('./.vscode/')
-    );
+    this.setUpVsCodeFiles();
   }
 
   // install() {
   //   this.npmInstall();
   // }
+
+
 };
--- generators/app/templates/ClientApp/src/app/app.module.ts
+++ generators/app/templates/ClientApp/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { DevExtremeModule } from 'devextreme-angular';
+// import { DevExtremeModule } from 'devextreme-angular';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -21,7 +21,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    DevExtremeModule,
+    //DevExtremeModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'fetch-data', component: FetchDataComponent },
